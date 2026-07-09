1a301347c31a374c0d5b67e12028eac746da9276
Amirvil <amir.vilensky@gmail.com>
2026-07-09 14:53:17 +0300

    Fix Connection

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 export default {
-  dbURL: 'mongodb://127.0.0.1:27017',
-  dbName: 'stay_db'
+  dbURL: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017',
+  dbName: process.env.DB_NAME || 'stay_db'
 }
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
+import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import dotenv from 'dotenv'
 import stayRoutes from './api/stay.routes.js'
-
-dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3030
